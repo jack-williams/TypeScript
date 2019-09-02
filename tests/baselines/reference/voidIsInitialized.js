e061e447e37225d12a809b631a0c6db14c5c7712
Jack Williams <jw@jackw.io>
2019-09-02 23:33:15 +0100

    Assume void variables are initialized

---
+++ tests/baselines/reference/voidIsInitialized.js
@@ -0,0 +1,14 @@
+//// [voidIsInitialized.ts]
+const x: void = undefined;
+
+if(typeof x === "undefined") {
+    x // no error
+}
+
+
+//// [voidIsInitialized.js]
+"use strict";
+var x = undefined;
+if (typeof x === "undefined") {
+    x; // no error
+}
---
+++ tests/cases/compiler/voidIsInitialized.ts
@@ -0,0 +1,7 @@
+// @strict: true
+
+const x: void = undefined;
+
+if(typeof x === "undefined") {
+    x // no error
+}
